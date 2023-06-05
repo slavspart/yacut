cac44c70ca100714faca02073dad661be16e5612
Святослав М <slavspart@yandex.ru>
2023-06-05 17:32:50 +0300

    s20(21) 2.2

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -1,3 +1,4 @@
+from http import HTTPStatus
 from urllib.parse import urlparse
 
 from werkzeug.exceptions import BadRequest
@@ -13,12 +14,12 @@ from .validators import latin_and_digits_length, is_unique
 def get_url(short):
     url = URLMap.query.filter_by(short=short).first()
     if url is None:
-        raise InvalidAPIUsage('Указанный id не найден', 404)
+        raise InvalidAPIUsage('Указанный id не найден', HTTPStatus.NOT_FOUND)
     # domain = urlparse(request.base_url).scheme + '://' + urlparse(request.base_url).netloc
     response_dict = url.to_dict()
     del (response_dict)['short_link']
     # response_dict['short_link'] = urlparse(request.base_url).scheme + '://' + urlparse(request.base_url).netloc + '/' + url.short
-    return jsonify(response_dict), 200
+    return jsonify(response_dict), HTTPStatus.OK
 
 
 @app.route('/api/id/', methods=['POST'])
@@ -26,17 +27,17 @@ def add_url():
     try:
         data = request.get_json(force=True)
     except BadRequest:
-        raise InvalidAPIUsage("Отсутствует тело запроса", 400)
+        raise InvalidAPIUsage("Отсутствует тело запроса", HTTPStatus.BAD_REQUEST)
     if 'url' not in data:
-        raise InvalidAPIUsage('"url" является обязательным полем!', 400)
+        raise InvalidAPIUsage('"url" является обязательным полем!', HTTPStatus.BAD_REQUEST)
     if not is_unique(data.get('custom_id')):
-        raise InvalidAPIUsage(f'Имя "{data.get("custom_id")}" уже занято.', 400)
+        raise InvalidAPIUsage(f'Имя "{data.get("custom_id")}" уже занято.', HTTPStatus.BAD_REQUEST)
     if not latin_and_digits_length(data.get('custom_id')):
-        raise InvalidAPIUsage('Указано недопустимое имя для короткой ссылки', 400)
+        raise InvalidAPIUsage('Указано недопустимое имя для короткой ссылки', HTTPStatus.BAD_REQUEST)
     url = URLMap()
     url.from_dict(data, 'url', 'custom_id')
     db.session.add(url)
     db.session.commit()
     response_dict = url.to_dict()
     response_dict['short_link'] = urlparse(request.base_url).scheme + '://' + urlparse(request.base_url).netloc + '/' + url.short
-    return jsonify(response_dict), 201
+    return jsonify(response_dict), HTTPStatus.CREATED
